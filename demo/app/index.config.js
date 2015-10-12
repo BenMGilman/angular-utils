51712df4a5bebb01d8a8c69966446b12cc530a0c
bgilman <bgilman@Ben-Gilman-Mac.local>
2015-10-12 15:03:57 -0500

    responsive and theming updates

--- demo/app/index.config.js
+++ demo/app/index.config.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -15,8 +15,21 @@
     toastr.options.positionClass = 'toast-top-right';
     toastr.options.preventDuplicates = true;
     toastr.options.progressBar = true;
-    
-    $mdThemingProvider.theme('default');
+
+    // Extend the themes with a few different colors
+    var redMap = $mdThemingProvider.extendPalette('red', {
+      '500': 'C52033'
+    });
+    var greyMap = $mdThemingProvider.extendPalette('grey', {
+      'A200': '9A999A'
+    });
+    // Register the new color palette maps
+    $mdThemingProvider.definePalette('redMap', redMap);
+    $mdThemingProvider.definePalette('greyMap', greyMap);
+    // Use that theme for the primary intentions
+    $mdThemingProvider.theme('default')
+      .primaryPalette('redMap')
+      .accentPalette('greyMap');
   }
 
 })();
